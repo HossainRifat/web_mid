a4d73c9572549104c4187a46237e840ac621eca6
HossainRifat <rh140025@gmail.com>
2022-12-09 13:39:51 +0600

    dashboard & security with notification,ip,loc.

--- app/Http/Controllers/BuyerController.php
+++ app/Http/Controllers/BuyerController.php
@@ -27,7 +27,8 @@ use Illuminate\Support\Facades\Hash;
 use function PHPUnit\Framework\isNull;
 use function Symfony\Component\VarDumper\Dumper\esc;
 
-class dashBoardModel{
+class dashBoardModel
+{
     var $user;
     var $post;
     var $order;
@@ -37,16 +38,26 @@ class dashBoardModel{
     var $myPost;
 }
 
-class securityModel{
+class securityModel
+{
     var $user;
     var $login;
+}
+
+class bidModel
+{
+    var $seller;
+    var $postId;
+    var $postTitle;
+    var $date;
+    var $amount;
 }
 
 class BuyerController extends Controller
 {
     public function RegistrationSubmit(Request $request)
     {
-        
+
         $data = [
             'first_name' => $request->first_name,
             'last_name' => $request->last_name,
@@ -66,7 +77,7 @@ class BuyerController extends Controller
         // session()->forget("validation_id");
         // session()->put("validation_id", $validation_id);
         // session()->save();
-        
+
 
         $details = [
             'title' => 'Welcome form RMG SOlution.',
@@ -79,8 +90,7 @@ class BuyerController extends Controller
 
         // return Redirect::to("https://mail.google.com/mail/");
 
-        return response($validation_id,200);
-        
+        return response($validation_id, 200);
     }
 
     public function Registration02(Request $request)
@@ -120,67 +130,67 @@ class BuyerController extends Controller
         session()->put("reg2", $jsonData);
         session()->save();
         return response("data saved", 200);
-    } 
+    }
 
-        // if ($request->account) {
-        //     $filename = date("d-m-Y_H-i-s") . '_account_' . session()->get('email') . '.' . $request->account->extension();
-        //     $filePath = $request->file('account')->storeAs('uploads', $filename, 'public');
-        //     $filePath2 = NULL;
-        //     if ($request->documents) {
-        //         $filename2 = date("d-m-Y_H-i-s") . '_documents_' . session()->get('email') . '.' . $request->documents->extension();
-        //         $filePath2 = $request->file('documents')->storeAs('uploads', $filename2, 'public');
-        //     }
-        //     if ($filePath) {
-        //         if ($filePath2) {
+    // if ($request->account) {
+    //     $filename = date("d-m-Y_H-i-s") . '_account_' . session()->get('email') . '.' . $request->account->extension();
+    //     $filePath = $request->file('account')->storeAs('uploads', $filename, 'public');
+    //     $filePath2 = NULL;
+    //     if ($request->documents) {
+    //         $filename2 = date("d-m-Y_H-i-s") . '_documents_' . session()->get('email') . '.' . $request->documents->extension();
+    //         $filePath2 = $request->file('documents')->storeAs('uploads', $filename2, 'public');
+    //     }
+    //     if ($filePath) {
+    //         if ($filePath2) {
 
-        //             if ($request->passport) {
-        //                 $data = [
-        //                     'nid' => $request->nid,
-        //                     'passport' => $request->passport,
-        //                     'phone' => $request->phone,
-        //                     'account' => $filename,
-        //                     'documents' => $filePath2,
-        //                 ];
-        //             } else {
-        //                 $data = [
-        //                     'nid' => $request->nid,
-        //                     'phone' => $request->phone,
-        //                     'account' => $filename,
-        //                     'documents' => $filePath2,
-        //                     'passport' => "NULL",
-        //                 ];
-        //             }
-        //         } else {
-        //             if ($request->passport) {
-        //                 $data = [
-        //                     'nid' => $request->nid,
-        //                     'passport' => $request->passport,
-        //                     'phone' => $request->phone,
-        //                     'account' => $filename,
-        //                     'documents' => "NULL",
-        //                 ];
-        //             } else {
-        //                 $data = [
-        //                     'nid' => $request->nid,
-        //                     'phone' => $request->phone,
-        //                     'account' => $filename,
-        //                     'documents' => "NULL",
-        //                     'passport' => "NULL",
-        //                 ];
-        //             };
-        //         }
+    //             if ($request->passport) {
+    //                 $data = [
+    //                     'nid' => $request->nid,
+    //                     'passport' => $request->passport,
+    //                     'phone' => $request->phone,
+    //                     'account' => $filename,
+    //                     'documents' => $filePath2,
+    //                 ];
+    //             } else {
+    //                 $data = [
+    //                     'nid' => $request->nid,
+    //                     'phone' => $request->phone,
+    //                     'account' => $filename,
+    //                     'documents' => $filePath2,
+    //                     'passport' => "NULL",
+    //                 ];
+    //             }
+    //         } else {
+    //             if ($request->passport) {
+    //                 $data = [
+    //                     'nid' => $request->nid,
+    //                     'passport' => $request->passport,
+    //                     'phone' => $request->phone,
+    //                     'account' => $filename,
+    //                     'documents' => "NULL",
+    //                 ];
+    //             } else {
+    //                 $data = [
+    //                     'nid' => $request->nid,
+    //                     'phone' => $request->phone,
+    //                     'account' => $filename,
+    //                     'documents' => "NULL",
+    //                     'passport' => "NULL",
+    //                 ];
+    //             };
+    //         }
 
-        //         $jsonData = json_encode($data);
-        //         //dd($jsonData);
-        //         session()->put("reg2", $jsonData);
-        //         session()->save();
-        //         //dd(session()->get('reg2'), session()->get('reg1'));
+    //         $jsonData = json_encode($data);
+    //         //dd($jsonData);
+    //         session()->put("reg2", $jsonData);
+    //         session()->save();
+    //         //dd(session()->get('reg2'), session()->get('reg1'));
 
-        //         return redirect()->route('Registration03');
-        //     } else {
-        //         return redirect()->route('Registration02');
-        //     }
-        // }
+    //         return redirect()->route('Registration03');
+    //     } else {
+    //         return redirect()->route('Registration02');
+    //     }
+    // }
 
     public function Registration03()
     {
@@ -194,7 +204,7 @@ class BuyerController extends Controller
         // //return response($jsondata,200);
         // $jsondata2 = session()->get('reg2');
         // $data2 = json_decode($jsondata2);
-      //  return response($data,200);
+        //  return response($data,200);
 
         $hash_password = Hash::make($data->password);
 
@@ -228,8 +238,8 @@ class BuyerController extends Controller
 
     public function BuyerDashboard(Request $request)
     {
-        $buyer = buyer::where("email","rh140035@gmail.com")->first();
-        if($buyer){
+        $buyer = buyer::where("email", "rh140035@gmail.com")->first();
+        if ($buyer) {
             $d = new dashBoardModel();
             $checkout = 0;
             $money = 0;
@@ -243,12 +253,20 @@ class BuyerController extends Controller
                     }
                 }
             }
-            $bid_info = new bid();
-            foreach($buyer->my_post as $item){
-                if($item->bid){
-                    foreach($item->bid as $item2){
-                        if($item2->status == "post"){
-                            $bid_info .= $item2;
+            $bid_info = array();
+            $b = new bidModel();
+            $i = 0;
+            foreach ($buyer->my_post as $item) {
+                if ($item->bid) {
+                    foreach ($item->bid as $item2) {
+                        if ($item2->status == "post") {
+                            $b->seller = $item2->seller->first_name . " " . $item2->seller->last_name;
+                            $b->postId = $item->id;
+                            $b->postTitle = $item->title;
+                            $b->amount = $item2->price;
+                            $b->date = $item2->bid_date;
+                            $bid_info[$i] = $b;
+                            $i++;
                         }
                     }
                 }
@@ -262,11 +280,9 @@ class BuyerController extends Controller
             $d->myPost = $bid_info;
             //dd($buyer);
             $data = json_encode($d);
-            return response($data,200);
-        }
-        else{
-            return response("Invalid Buyer",401);
-
+            return response($data, 200);
+        } else {
+            return response("Invalid Buyer", 401);
         }
     }
 
@@ -312,19 +328,17 @@ class BuyerController extends Controller
     {
         $token = $request->header("Authorization");
         $token = json_decode($token);
-       // return response($token,200);
-        if($token){
-            $check_token = login::where('token',$token->access_token)->where("logout_time",NULL)->first();
-            if($check_token){
-                $user = all_user::where("id",$check_token->all_users_id)->first();
-                if($user){
-                    $buyer = buyer::where("email",$user->email)->first();
-                    if($buyer){
-                        return response($buyer,200);
-                    }
-                    else{
-                        return response("Invalid Buyer",401);
-
+        // return response($token,200);
+        if ($token) {
+            $check_token = login::where('token', $token->access_token)->where("logout_time", NULL)->first();
+            if ($check_token) {
+                $user = all_user::where("id", $check_token->all_users_id)->first();
+                if ($user) {
+                    $buyer = buyer::where("email", $user->email)->first();
+                    if ($buyer) {
+                        return response($buyer, 200);
+                    } else {
+                        return response("Invalid Buyer", 401);
                     }
                 }
             }
@@ -410,23 +424,22 @@ class BuyerController extends Controller
     {
         $token = $request->header("Authorization");
         $token = json_decode($token);
-       // return response($token,200);
-        if($token){
-            $check_token = login::where('token',$token->access_token)->where("logout_time",NULL)->first();
-            if($check_token){
-                $user = all_user::where("id",$check_token->all_users_id)->first();
+        // return response($token,200);
+        if ($token) {
+            $check_token = login::where('token', $token->access_token)->where("logout_time", NULL)->first();
+            if ($check_token) {
+                $user = all_user::where("id", $check_token->all_users_id)->first();
 
-                if($user){
-                    $user2 = login::where("all_users_id",$user->id)->where("logout_time",NULL)->get();
+                if ($user) {
+                    $user2 = login::where("all_users_id", $user->id)->where("logout_time", NULL)->get();
                     $buyer = buyer::where('email', $user->email)->first();
-                    if($buyer){
+                    if ($buyer) {
                         $s = new securityModel();
                         $s->user = $buyer;
                         $s->login = $user2;
                         $data = json_encode($s);
-                        return response($data,200);
+                        return response($data, 200);
                     }
-                    
                 }
             }
         }
@@ -446,36 +459,55 @@ class BuyerController extends Controller
 
     public function ChangePass(Request $request)
     {
-        $this->validate(
-            $request,
-            [
-                "old_password" => ["required", "max:50", "min:8", new ChangePassRule],
-                "password" => ["required", "max:50", "min:8"],
-                "password_confirmation" => ["required", "min:8", "max:50", "same:password"],
+        $token = $request->header("Authorization");
+        $token = json_decode($token);
+        // return response($token,200);
+        if ($token) {
+            $check_token = login::where('token', $token->access_token)->where("logout_time", NULL)->first();
+            if ($check_token) {
+                $user = all_user::where("id", $check_token->all_users_id)->first();
 
-            ],
-            [
-                "password.min" => "Password must be at least 8 character.",
-                "password_confirmation.min" => "Password must be at least 8 character.",
-                "password_confirmation.same" => "Password did not match."
-            ]
+                if ($user) {
+                    $buyer = buyer::where('email', $user->email)->first();
+                    if ($buyer) {
 
-        );
 
-        $user = buyer::where('email', session()->get("email"))->first();
-        $all_user = all_user::where('email', session()->get("email"))->first();
+                        if (Hash::check($request->oldPassword, $buyer->password)) {
+                            $buyer->password = Hash::make($request->newPassword);
+                            $buyer->save();
 
-        $hash_pass = Hash::make($request->password);
-        if ($user) {
-            $user->password = $hash_pass;
-            $user->save();
+                            $user->password = Hash::make($request->newPassword);
+                            $user->save();
+                            return response("Changed", 200);
+                        } else {
+                            return response("Password not match", 203);
+                        }
+                    } else {
+                        return response("invalid", 401);
+                    }
+                } else {
+                    return response("invalid", 401);
+                }
+            } else {
+                return response("invalid", 401);
+            }
 
-            $all_user->password = $hash_pass;
-            $all_user->save();
-            return redirect()->route("BuyerDashboard");
-        } else {
-            return redirect()->route("Login");
+            // $user = buyer::where('email', session()->get("email"))->first();
+            // $all_user = all_user::where('email', session()->get("email"))->first();
+
+            // $hash_pass = Hash::make($request->password);
+            // if ($user) {
+            //     $user->password = $hash_pass;
+            //     $user->save();
+
+            //     $all_user->password = $hash_pass;
+            //     $all_user->save();
+            //     return redirect()->route("BuyerDashboard");
+            // } else {
+            //     return redirect()->route("Login");
+            // }
         }
+        return response("nit", 401);
     }
 
     public function ValidationEmail()
--- routes/api.php
+++ routes/api.php
@@ -20,13 +20,13 @@ Route::post('/login', [LoginController::class, 'LoginSubmit']);
 Route::get('/all', [LoginController::class, 'LoginAll']);
 Route::get('/t', [LoginController::class, 't']);
 Route::get('/buyer/dashboard', [BuyerController::class, 'BuyerDashboard'])->name("BuyerDashboard");
-Route::get('/buyer/profile/get', [BuyerController::class, 'Profile'])->name("BuyerDashboard")->middleware('ValidToken');
-Route::get('/buyer/security/get', [BuyerController::class, 'Security'])->name("BuyerDashboard")->middleware('ValidToken');
+Route::get('/buyer/profile/get', [BuyerController::class, 'Profile'])->middleware('ValidToken');
+Route::get('/buyer/security/get', [BuyerController::class, 'Security'])->middleware('ValidToken');
+Route::post('/buyer/security/update', [BuyerController::class, 'ChangePass']);
+
 
 
 
 Route::post('/buyer/registration1', [BuyerController::class, 'RegistrationSubmit']);
 Route::post('/buyer/registration2', [BuyerController::class, 'Registration02Submit']);
 Route::post('/buyer/registration3', [BuyerController::class, 'Registration03Submit']);
-
-
